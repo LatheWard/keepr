d451bc2faa4ca443a09ae69c4d8a057893aeac23
Lathe Ward <lathelward@gmail.com>
2025-01-25 18:19:25 -0600

    Use the time library better, add launch.json for debug

--- stopwatch.py
+++ stopwatch.py
@@ -3,23 +3,17 @@ import time
 ## Expected Output
 ## Timestamp(?), Team, Project, Issue/Ticket Number, Notes
 
+def clock():
+    print("Press enter to start")
+    input()
+    start_time = time.perf_counter()
 
-def main():
-    counter = 0
-    def startDay():
-        global counter
-        for i in range(10):
-            # tt = datetime.fromtimestamp(counter)
-            display_stamp = time.strftime("%H:%M:%S", time.gmtime(counter))
-            display=display_stamp
-            print(display)
-            counter += 1
-            time.sleep(1)
-        return None 
-    is_starting = input("Ready?")
-    if is_starting == 'Y':
-        print("Starting....")
-        startDay()
+    print("Press enter to end")
+    input()
+    end_time = time.perf_counter()
+    elapsed = end_time - start_time
+    print(time.strftime("%H:%M:%S", time.gmtime(elapsed)))
     return None
 
-main()
+if __name__ == "__main__":
+    clock()
