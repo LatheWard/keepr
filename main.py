4631e766c8dd8979d48f3a578457f959137821d8
Lathe Ward <lathelward@gmail.com>
2025-04-12 01:06:46 -0500

    Add all functions to main.py, learn some of Kivy's language

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import time
+import kivy
 
-## Timestamp, Team, Project, Issue/Ticket Number, Notes
-def main():
+kivy.require("2.3.1")
+import time
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.config import Config
+
+
+class RunScreen(Widget):
+    pass
+
+
+class KeeprApp(App):
+    Config.set("graphics", "width", "400")
+    Config.set("graphics", "height", "150")
+
+    def build(self):
+        return RunScreen()
+
     def log_activity(time):
         team = input("Team?: ")
         project = input("Project?: ")
@@ -23,18 +40,18 @@ def main():
         end_time, elapsed = 0, 0
         while active:
             i2 = input("Enter note or end?:")
-            if i2 == "":
+            if button_press_off:
                 end_time = time.perf_counter()
                 elapsed = end_time - start_time
-                print(time.strftime("%H:%M:%S", time.gmtime(elapsed)), "Log: ", logs)
+                # print(time.strftime("%H:%M:%S", time.gmtime(elapsed)), "Log: ", logs)
                 active = False
             else:
                 end_time = time.perf_counter()
                 elapsed = end_time - start_time
                 log = log_activity(time.strftime("%H:%M:%S", time.gmtime(elapsed)))
                 logs.append(log)
-                print(time.strftime("%H:%M:%S", time.gmtime(elapsed)), "Last Log: ", log)
-    clock()        
+                # print(time.strftime("%H:%M:%S", time.gmtime(elapsed)), "Last Log: ", log)
+
 
 if __name__ == "__main__":
-    main()
+    KeeprApp().run()
