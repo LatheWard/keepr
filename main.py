6def6b5d6e33d30d0913f8a1dbb5c215d195e437
Lathe Ward <lathelward@gmail.com>
2025-04-19 21:37:23 -0500

    Use custom tkinter instead

--- main.py
+++ main.py
@@ -1,57 +1,45 @@
-import kivy
-
-kivy.require("2.3.1")
+import customtkinter
 import time
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.config import Config
 
 
-class RunScreen(Widget):
-    pass
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.active = False
+        self.logs = []
+        self.title("Keepr")
+        self.geometry("400x150")
+        self.button = customtkinter.CTkButton(self, text="testing123")
+        self.button.grid(row=0, column=0, padx=20, pady=20)
+        def log_activity():
+            team = input("Team?: ")
+            project = input("Project?: ")
+            ticket = input("Issue/Ticket?: ")
+            notes = input("Notes?:  ")
+            activity_log = {
+                "Timestamp": time.strftime("%H:%M:%S", time.gmtime(elapsed)),
+                "Team": team,
+                "Project": project,
+                "Issue/Ticket": ticket,
+                "Notes": notes,
+            }
+            self.logs.append(activity_log)
 
+        def clock():
+            active = not active
+            start_time = time.perf_counter()
+            end_time, elapsed = 0, 0
+            while active:
+                if button_press_off:
+                    end_time = time.perf_counter()
+                    elapsed = end_time - start_time
+                    # print(time.strftime("%H:%M:%S", time.gmtime(elapsed)), "Log: ", logs)
+                else:
+                    end_time = time.perf_counter()
+                    elapsed = end_time - start_time
+                    log_activity()
+                    # print(time.strftime("%H:%M:%S", time.gmtime(elapsed)), "Last Log: ", log)
+    
 
-class KeeprApp(App):
-    Config.set("graphics", "width", "400")
-    Config.set("graphics", "height", "150")
-
-    def build(self):
-        return RunScreen()
-
-    def log_activity(time):
-        team = input("Team?: ")
-        project = input("Project?: ")
-        ticket = input("Issue/Ticket?: ")
-        notes = input("Notes?:  ")
-        activity_log = {
-            "Timestamp": time,
-            "Team": team,
-            "Project": project,
-            "Issue/Ticket": ticket,
-            "Notes": notes,
-        }
-        return activity_log
-
-    def clock():
-        active = True
-        logs = []
-        start_time = time.perf_counter()
-        end_time, elapsed = 0, 0
-        while active:
-            i2 = input("Enter note or end?:")
-            if button_press_off:
-                end_time = time.perf_counter()
-                elapsed = end_time - start_time
-                # print(time.strftime("%H:%M:%S", time.gmtime(elapsed)), "Log: ", logs)
-                active = False
-            else:
-                end_time = time.perf_counter()
-                elapsed = end_time - start_time
-                log = log_activity(time.strftime("%H:%M:%S", time.gmtime(elapsed)))
-                logs.append(log)
-                # print(time.strftime("%H:%M:%S", time.gmtime(elapsed)), "Last Log: ", log)
-
-
-if __name__ == "__main__":
-    KeeprApp().run()
+app = App()
+app.mainloop
